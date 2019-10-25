4f98bbe3f90d5b6e0a45113a8d3f4b1a3e84d7e0
Shaik Adil <shaikadil86@gmail.com>
2019-10-25 18:53:17 -0400

    Create Join_util.ex

---
+++ lib/Join_util.ex
@@ -0,0 +1,27 @@
+defmodule Joiner do
+    def start(current_node_id,table,new_node) do
+
+
+     #Get Level where the new node belongs which will be your row_no
+     {level,new_node}=Tapestry.RoutingTable.get_prefix_match(current_node_id, new_node)
+     row_no=level
+
+     #getting column_no from the new_nodes name
+     column_no=String.at(new_node, level)
+
+     #get current value which is in the table
+     val=table[row_no][column_no]
+
+     #update the value
+     IO.inspect("Your old val:"<>val)
+     if val==nil do
+      IO.inspect("replace with: "<>new_node)
+      Tapestry.RoutingTable.update_table(table, row_no, column_no, new_node)
+     else
+      replacement=Tapestry.RoutingTable.get_closest_node(current_node_id, [val,new_node])
+      IO.inspect("replace with:"<>replacement)
+      Tapestry.RoutingTable.update_table(table, row_no, column_no, replacement)
+     end
+
+    end
+end
